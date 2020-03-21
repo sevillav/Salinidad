90dfe3104849c03ffcba0da884b4ac7dee33082d
Victor Sevilla <samoy314@gmail.com>
2020-03-20 23:13:07 -0400

    Se creo el objeto "Horizontes"

--- prueba1.R
+++ prueba1.R
@@ -1,1 +1,12 @@
-#cvxcvxcz
+setwd("C:/Sevilla/Mario/Venezuela/Salinidad/Salinidad")
+getwd()
+
+horizontes <- read.table("C:/Sevilla/Mario/Venezuela/Salinidad/Salinidad/Tablas/Sal_horizontes.csv", sep=",", dec = ".", header=TRUE)
+names(horizontes)
+summary(horizontes)
+horizontes1 <- horizontes[,-(42:51)]
+summary(horizontes1)
+str(horizontes1)
+
+dim(horizontes1)
+boxplot(horizontes1[,21:41])
